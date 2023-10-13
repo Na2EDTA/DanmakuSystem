abcccf216ed0527174d223e969ab2a1cef817bd7
Na2EDTA <2637797530@qq.com>
2023-10-13 22:47:07 +0800

    23/10/13 尝试增加泛型create

--- Assets/Scripts/Pool.cs
+++ Assets/Scripts/Pool.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEditor;
+using System.IO;
 
 public class Pool : MonoBehaviour
 {
@@ -82,5 +85,79 @@ public class Pool : MonoBehaviour
         string cname = obj.name;
         mainPool[cname.Remove(cname.Length - 7)].Enqueue(obj);
         return;
+    }
+
+    /*-------------------------------------------------------------------------------*/
+
+    [SerializeField] public Danmaku.SerializeExtension.Dictionary<System.Type, Queue<GameObject>> objectPool = new();
+    Dictionary<System.Type, GameObject> objectList = new();
+
+#if UNITY_EDITOR
+    public void Init()
+    {
+        List<GameObject> prefabs = new();
+        FindPrefabsInDirectroy("Assets/Prefabs/", out prefabs);
+        
+        if (prefabs.Any())
+        {
+            for (int i = 0; i < prefabs.Count; i++)
+            {
+                TryGetComponent<DanmakuObject>(out var d);
+                System.Type type = d.GetType();
+                if (objectPool.Keys.Contains(type))
+                    continue;
+                else
+                {
+                    objectPool.Add(type, new());
+                    objectList.Add(type, prefabs[i]);
+                }
+            }
+        }
+    }
+
+    static void FindPrefabsInDirectroy(string path, out List<GameObject> output)
+    {
+        output = new();
+
+        if (string.IsNullOrEmpty(path)) throw new System.ArgumentException("Path is null or empty");
+        string[] subfolders = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+        string[] guids = null;
+        string[] assetPaths = null;
+        for (int i = 0; i < subfolders.Length; i++)
+        {
+            guids = AssetDatabase.FindAssets("t:Prefab");
+            assetPaths = new string[guids.Length];
+            //sth
+        }
+
+        output = null;
+    }
+#endif
+
+    void Fill<T>(int count = 50)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            var obj = Instantiate(objectList[typeof(T)], transform);
+            Dispose<T>(obj);
+        }
+    }
+
+    public GameObject Create<T>(Vector2 pos, params object[] ps) where T: DanmakuObject
+    {
+        Queue<GameObject> queue = objectPool[typeof(T)];
+        if (queue.Count < 10) Fill<T>();
+        var obj = queue.Dequeue();
+        obj.transform.position = pos;
+        obj.SetActive(true);
+        obj.GetComponent<SpriteRenderer>().sortingOrder = order;
+        order++;
+        return obj;
+    }
+
+    public void Dispose<T>(GameObject obj)
+    {
+        obj.SetActive(false);
+        objectPool[typeof(T)].Enqueue(obj);
     }
 }
